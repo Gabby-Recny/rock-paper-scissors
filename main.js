075641597436267bc185bfa1b5b327a403b9ad78
Gabby Recny <gjtrecny@gmail.com>
2021-11-11 14:46:40 -0600

    Complete basic game logic without DOM

--- main.js
+++ main.js
@@ -7,35 +7,66 @@ var classicChoice = document.getElementById("classicChoice");
 var computerChoice;
 var userChoice;
 
-classicIcons.addEventListener("click", function(button) {
-  var userChoice = button.id
+classicChoice.addEventListener("click", playGame)
+
+rock.addEventListener("click", function() {
+  userChoice = rock.id;
   console.log(userChoice)
 })
+
+scissors.addEventListener("click", function() {
+  userChoice = scissors.id;
+  console.log(userChoice)
+});
+
+paper.addEventListener("click", function() {
+  userChoice = paper.id;
+  console.log(userChoice)
+})
+
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
+
 
 function accessComputerChoice() {
   var choices = ["rock", "paper", "scissors"];
   computerChoice = choices[getRandomIndex(choices)]
   console.log(computerChoice)
 }
+accessComputerChoice();
 
-function accessUserChoice(button) {
-  var userChoice = classicIcons.id;
-  console.log(userChoice)
-}
 
-// Game logic
-// if (userChoice === computerChoice ) {
-//   console.log("it's a draw")
-// } else if (userChoice === "rock" && computerChoice === "scissors")  {
-//   console.log("User wins")
-// } else if (userChoice === "paper" && computerChoice === "rock") {
-//    console.log("User  wins");
-// } else if (userChoice === "scissors" && computerChoice === "paper") {
-//    console.log("User wins")
-// } else {
-//    console.log("Computer wins")
+// classicIcons.addEventListener("click", function() {
+//   accessUserChoise(event)
+// })
+// function accessUserChoise(event) {
+//   if (event.target.id === "scissors") {
+//     userChoice = scissors.id;
+//     console.log(userChoice)
+//   } else if (event.target.id === "rock") {
+//     userChoice = rock.id;
+//     console.log(userChoice)
+//   } else if (event.target.id === "paper") {
+//       userChoice = paper.id;
+//       console.log(userChoice)
+//   }
 // }
+
+
+
+
+function playGame() {
+  if (userChoice === computerChoice ) {
+    console.log("it's a draw")
+  } else if (userChoice === "rock" && computerChoice === "scissors")  {
+    console.log("User wins")
+  } else if (userChoice === "paper" && computerChoice === "rock") {
+     console.log("User  wins");
+  } else if (userChoice === "scissors" && computerChoice === "paper") {
+     console.log("User wins")
+  } else {
+     console.log("Computer wins")
+  }
+}
